052d0625a12eed8ddce60cd04d610f68d4ab0625
Sevalino Elfata <sevalinoelfata8@gmail.com>
2025-07-31 10:37:56 +0700

    `Added meta tag for Google site verification and updated SEO metadata`

--- src/seo.js
+++ src/seo.js
@@ -11,6 +11,7 @@ function Seo() {
   />
   <link rel="canonical" href="https://tehdeveloper.com/" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="google-site-verification" content="3_xlx2SJLCOWbUlveL_X7se4q4OTMgyaVadCoZzMxts" />
           
            <meta property="og:type" content="website" />
   <meta property="og:url" content="https://tehdeveloper.com/" />
